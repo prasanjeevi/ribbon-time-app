88715b5fe7ee36914e8b4d73f8a1b2095bc71bec
Sanjeevi <prasanjeevi@gmail.com>
2017-08-07 14:23:22 +0530

    spring init

---
+++ src/main/java/com/cloudier/ribbontimeapp/RibbonTimeAppApplication.java
@@ -0,0 +1,12 @@
+package com.cloudier.ribbontimeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonTimeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonTimeAppApplication.class, args);
+	}
+}
