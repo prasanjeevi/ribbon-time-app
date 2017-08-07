4570c2266725db497d931c9a5ed4bdf9b06acccf
Sanjeevi <prasanjeevi@gmail.com>
2017-08-07 16:23:08 +0530

    removed service discovery

--- src/main/java/com/cloudier/ribbontimeapp/RibbonTimeAppApplication.java
+++ src/main/java/com/cloudier/ribbontimeapp/RibbonTimeAppApplication.java
@@ -4,8 +4,8 @@ import javax.inject.Inject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@RibbonClient(name = "time-service")
 @RestController
 public class RibbonTimeAppApplication {
 
